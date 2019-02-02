979924d435fc1bd18a7e7af13b6bf698fb57f578
MeteorRyan <rjeasley88@gmail.com>
2019-02-02 16:51:16 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Require express
+const express = require('express')
+// Create a new server instance
+const server = express()
+// Tell express to server static content
+// from the public folder
+server.use(express.static('public', {
+  extensions: ['html', 'htm', 'jpg']
+}))
+// Grab the port off the environment
+// but fallback to the value 5000 if
+// the user never set the port
+const { PORT = 5000 } = process.env
+
+server.listen(PORT, () => console.log(
+  "Listening at http://localhost:%s", PORT
+))
